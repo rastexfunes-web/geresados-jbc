5d4d2e0c4adb54137bb30513dd6f972aa850033c
rastexfunes-web <rastexfunes@gmail.com>
2026-08-10 13:13:41 -0300

    Add files via upload

--- control-egresados/api/crear-preference.js
+++ control-egresados/api/crear-preference.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
     const preference = new Preference(client);
 
     const appUrl = `https://${req.headers.host}`;
-    const backUrl = `${appUrl}/colegios/${colegioId}/alumnos/${alumnoId}`;
+    const backUrl = `${appUrl}/pago-realizado`;
 
     const result = await preference.create({
       body: {
